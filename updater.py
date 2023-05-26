5f4c6752cb37362440215f1b16c677fc9fd5797b
brolando94 <47872050+brolando94@users.noreply.github.com>
2023-05-25 22:49:14 -0700

    minor updates

--- updater.py
+++ updater.py
@@ -17,7 +17,7 @@ import numpy as np
 from os import path
 
 # how many days back you want to run. Ideally you set it to run everyday pulling yesterdays scores
-yesterday = str(date.today() - timedelta(days=3))
+yesterday = str(date.today() - timedelta(days=0))
 
 # game files path
 game_path = path.dirname(path.abspath(__file__)) + '/game_files/'
@@ -31,7 +31,7 @@ else:
 
 current_data = pd.read_excel(input_path, dtype=str).replace({np.nan: None})
 
-current_data = current_data.set_index('team')
+current_data = current_data.set_index('mapping')
 
 url = f"https://www.espn.com/mlb/scoreboard/_/date/{yesterday.replace('-', '')}"
 response = requests.get(url=url)
@@ -43,9 +43,9 @@ games = content.xpath("//div[@class='Scoreboard__Column flex-auto Scoreboard__Co
 
 for i in range(len(games)*2):
     try:
-        team = content.xpath("(//ul[@class='ScoreboardScoreCell__Competitors']/li/div/div/a/"
-                             "div[@class='ScoreCell__TeamName ScoreCell__TeamName--shortDisplayName truncate db'])"
-                             f"[{i + 1}]/text()")[0]
+        mapping = content.xpath("(//ul[@class='ScoreboardScoreCell__Competitors']/li/div/div/a/div[@class="
+                                "'ScoreCell__TeamName ScoreCell__TeamName--shortDisplayName truncate db'])"
+                                f"[{i + 1}]/text()")[0]
 
         score = content.xpath("(//div[@class='ScoreboardScoreCell_Linescores baseball flex justify-end'])"
                               f"[{i + 1}]/div[1]/text()")[0]
@@ -54,12 +54,12 @@ for i in range(len(games)*2):
         continue
 
     score = int(score)
-    if 0 <= score <= 14:
-        if current_data.loc[[team]][score][0] is None:
-            current_data.at[f'{team}', score] = f"{yesterday[5:].replace('-', '/')}"
+    if 0 <= score < 14:
+        if current_data.loc[[mapping]][score][0] is None:
+            current_data.at[f'{mapping}', score] = f"{yesterday[5:].replace('-', '/')}"
 
 current_data.reset_index(drop=False, inplace=True)
-current_data = current_data[['name', 'team', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]]
+current_data = current_data[['mapping', 'team', 'owner', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]]
 
 writer = pd.ExcelWriter(output_path, engine='xlsxwriter')
 current_data.to_excel(writer, index=False)
