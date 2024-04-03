5a912c155280505a650c09f8b2cd5377aba8a8d2
brolando94 <47872050+brolando94@users.noreply.github.com>
2024-04-02 21:43:07 -0500

    put the receivers in the environment variables and cleaned up some stuff

--- table_output.py
+++ table_output.py
@@ -18,32 +18,34 @@ from os import environ as env
 from pathlib import Path
 from emailer import send_email
 import sys
-
-
-def zebra_stripe(data):
-    s = data.index % 2 != 0
-    s = pd.concat([pd.Series(s)] * data.shape[1], axis=1)  # 6 or the n of cols u have
-    z = pd.DataFrame(np.where(s, 'background-color:#D1D1D1', ''), index=data.index, columns=data.columns)
-    return z
-
-
-if len(sys.argv) < 2:
-    exit("Pass in at least 1 Email Recipient")
-
-# for email
-receivers = list(sys.argv[1:])
+import re
 
 
 # environment variables
 if load_dotenv(r'vars\vars.env') is False:
     with open(r'vars\vars.env', 'w') as file:
-        file.write("email=''\nemail_pwd=''\nerror_email=''")
+        file.write("email=''\nemail_pwd=''\nreceivers=''")
     exit('Failed to load environment vars. Fill out vars.env')
+
+# for email
+receivers = str(env.get('receivers'))
 email = str(env.get('email'))
+password = str(env.get('email_pwd'))
+
+if receivers == '':
+    exit('receivers variable empty. Need at least one receiver email filled out in vars.env')
+
+if email == '':
+    exit('email variable empty. Fill out sender email in vars.env')
+
+if password == '':
+    exit('email_pwd variable empty. Fill out sender email password in vars.env')
+
 email_credentials = {
-    "host": "smtp.gmail.com", "port": "465", "login": email,
-    'pwd': f"{env.get('email_pwd')}", "sender": email
+    "host": "smtp.gmail.com", "port": "465", "login": email, 'pwd': password, "sender": email
 }
+
+receivers = re.split(r',|\s', receivers)
 
 yesterday = str(date.today() - timedelta(days=1))
 
@@ -54,41 +56,7 @@ subject = f'MLB Game Through {yesterday}'
 file_path = path.dirname(path.abspath(__file__)) + '/game_files/current_game.xlsx'
 
 if not path.exists(file_path):
-    exit('No current game to format')
-
-current_game = pd.read_excel(file_path, dtype=str).replace({np.nan: None})
-
-current_game['total'] = current_game['total'].astype('int')
-
-current_game_style = current_game.style \
-    .apply(zebra_stripe, axis=None) \
-    .set_table_styles([{'selector': 'td',
-                        'props': 'border: 2px solid black; border-collapse: collapse'},
-                       {'selector': 'thead, th',
-                        'props': 'background-color: #D3D3D3; color: black; '
-                        'border-right: 2px solid black; '
-                        'border-collapse: collapse; '
-                        'padding: 15px'}
-                       ]) \
-    .set_properties(**{'padding': '15px', 'border': '2px solid black', 'border-collapse': 'collapse'})
-
-current_game_style.to_excel(file_path, index=False)
-current_game_html = current_game_style.hide(axis='index').to_html().replace('None', '')
-
-# body for email
-html = f"""\
-            <html>
-                <head>Hello,<br></br>\nSee Below or Attached Excel File\n </head>
-                    <body>
-                        <div>
-                            <br></br>
-                            <b>\nCurrent Game:</b>
-                            {current_game_html}
-                            <br></br>
-                        </div>
-                    </body>
-                </html>
-        """
+    exit('No current game to email. Run Updater.py to get started')
 
 # send email
 send_email(credentials=email_credentials, subject=subject, body="See Attached", receivers=receivers,
